d5bd0fca1acabcc35b6f4af5300e96cf8c2a1311
JARVIS243 <jarvisai0025@gmail.com>
2025-07-08 20:58:27 +0530

    export.py

--- export.py
+++ export.py
@@ -6,12 +6,20 @@ import os
 
 # --- Export as TXT ---
 def export_to_txt(filename, content):
+    folder = os.path.dirname(filename)
+    if folder and not os.path.exists(folder):
+        os.makedirs(folder)
+
     with open(filename, "w", encoding="utf-8") as f:
         f.write(content)
     return filename
 
 # --- Export as PDF ---
 def export_to_pdf(filename, content):
+    folder = os.path.dirname(filename)
+    if folder and not os.path.exists(folder):
+        os.makedirs(folder)
+
     c = canvas.Canvas(filename, pagesize=letter)
     width, height = letter
     x, y = 40, height - 50
